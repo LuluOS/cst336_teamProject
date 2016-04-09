6c5cc7a9ae46bbac4376a550792ad55e56e52cda
ricomartinez <josemartinez2@csumb.edu>
2016-04-09 03:16:17 +0000

    Here is the database to use guys

---
+++ Main.php
@@ -0,0 +1,10 @@
+<?php 
+session_start(); //start or resume an existing session 
+
+include 'dbConn.php'; 
+
+$connection = dbConnect(); 
+    $sql = 
+             
+    
+?>
---
+++ dbConn.php
@@ -0,0 +1,29 @@
+<?php
+function dbConnect(){
+// Set the Cloud 9 MySQL related information...this is set in stone by C9!
+$servername = getenv('IP');
+$dbPort = 3306;
+
+// Which database (the name of the database in phpMyAdmin)?
+$database = "OtterExpress1";
+
+// My user information...I could have prompted for password, as well, or stored in the
+// environment, or, or, or (all in the name of better security)
+$username = getenv('C9_USER');
+$password = "1234";
+
+// Establish the connection and then alter how we are tracking errors (look those keywords up)
+$dbConn = new PDO("mysql:host=$servername;port=$dbPort;dbname=$database", $username, $password);
+$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+return $dbConn;
+}
+
+function getConn($sql){
+    global $connection;
+    $statement = $connection->prepare($sql);
+    $statement->execute();
+    $records = $statement-> fetchALL (PDO::FETCH_ASSOC);
+    return $records;
+}
+
+?>
